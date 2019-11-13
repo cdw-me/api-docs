9d3a78a6c86bcc0b729c3e7857cc317162b0cf2f
cdw-me <cdw.me@qq.com>
2019-11-13 15:39:30 +0800

    web端初始化

---
+++ web/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <a-locale-provider :locale="zh_CN">
+    <div id="app">
+      <Header></Header>
+    </div>
+  </a-locale-provider>
+</template>
+
+<script>
+import zh_CN from 'ant-design-vue/lib/locale-provider/zh_CN'
+import Header from './components/Header'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      zh_CN,
+    }
+  },
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
